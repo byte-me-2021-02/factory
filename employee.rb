c8e376472e9d2b2c4705bdad62ebd9df0aef0378
brian <riceb53@gmail.com>
2021-02-16 19:14:23 -0500

    model employee as array, and then hash

---
+++ employee.rb
@@ -0,0 +1,52 @@
+# model an employee using ruby
+
+# array
+# employee1 = ["Majora", "Carter", 80000, true]
+# p employee
+
+# p employee1[0] + " " + employee1[1] + " " + "makes $" + employee1[2].to_s + " per year."
+# p "Majora Carter Makes $80000 per year."
+
+# employee2 = ["Danilo", "Campos", 100000, false]
+
+# p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
+
+
+
+# hash
+employee1 = {"last_name" => "Carter", "salary" => 80000, "active" => true, "first_name" => "Majora"}
+# employee2 = {:first_name => "Danilo", :last_name => "Campos", :salary => 100000, :active => false}
+# fancy version of the above line
+employee2 = {first_name: "Danilo", last_name: "Campos", salary: 100000, active: false}
+# p employee2
+
+
+# what is the easies update i could do?
+# chaging the name?
+
+
+
+
+p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
+p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
+
+
+
+
+
+# What's better? hash or array? why?
+
+
+
+
+
+
+# object
+
+
+
+
+
+# What do we need to do to make this?
+
+
