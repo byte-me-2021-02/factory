8b6d61831615e4a075ca7dc4bc5aa8ee4164db4a
brian <riceb53@gmail.com>
2021-02-16 19:27:10 -0500

    model employee using class

--- employee.rb
+++ employee.rb
@@ -27,21 +27,34 @@ employee2 = {first_name: "Danilo", last_name: "Campos", salary: 100000, active: 
 
 
 
-p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
-
-
-
-
+# p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
 
 # What's better? hash or array? why?
 
 
-
-
-
-
 # object
+# OOP
+
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+    # p "in initialize"
+  end
+
+  def print_info
+    p "#{@first_name} #{@last_name} makes $#{@salary} per year."
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee1.print_info
+
+employee2 = Employee.new("Danilo", "Campos", 100000, false)
+employee2.print_info
 
 
 
